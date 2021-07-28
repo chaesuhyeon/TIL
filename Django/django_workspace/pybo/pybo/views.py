74e304fffdc23634944ebafa13d275af82812aed
chaesuhyeon <ddd8177@gmail.com>
2021-07-28 22:03:24 +0900

    django pybo2

--- Django/django_workspace/pybo/pybo/views.py
+++ Django/django_workspace/pybo/pybo/views.py
@@ -7,6 +7,7 @@ from django.views import generic
 
 from pybo.forms import QuestionForm
 from pybo.models import Question
+from django.contrib.auth.decorators import login_required
 
 #pybo 목록 출력
 
@@ -31,20 +32,23 @@ class DetailView(generic.DetailView):
     model = Question
 
 # question_id의 Answer 객체 save
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now(), author=request.user)
     # answer = Answer(question = Question.objects.get(id=question_id),content= request.POST.get('content'),create_date=timezone())
     # answer.save()
     return redirect('pybo:detail', pk=question.id)
 
 # question 등록화면 : get으로 들어오면 입력화면만 제공 , post로 요청들어오면 form(subject,content) 데이터 model 저장
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)   # subject = request.POST['subject'] content = request.POST['content']
         if form.is_valid():                 # input data valid check : if subject != null && content != null 와 같이 하나하나 다써서 valid 검사를 해줘야함
             question = form.save(commit=False) # question = Question(subject=subject, content = content, create_date = timezone.now())
             question.create_date = timezone.now()
+            question.author = request.user
             question.save()
             return redirect('pybo:index')
     else:

6c7fa858a50e4059914b14fe96fb68fc82b1b8fd
chaesuhyeon <ddd8177@gmail.com>
2021-08-03 14:09:41 +0900

    pybo q modify

--- Django/django_workspace/pybo/pybo/urls.py
+++ Django/django_workspace/pybo/pybo/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(),name='detail'),
     path('answer/create/<int:question_id>',views.answer_create,name='answer_create'),
     path('question/create/',views.question_create,name='question_create'),
+    path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),
 ]

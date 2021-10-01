10c81a52893adaa87617818beb863e0034cbe996
chaesuhyeon <ddd8177@gmail.com>
2021-10-01 17:54:29 +0900

    book-rest

--- Django/django_workspace/todos/book/models.py
+++ Django/django_workspace/todos/book/models.py
@@ -5,13 +5,13 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    object = models.Manager()  # 인식이 안되는 경우가 있어서 그냥 넣어줌
+    objects = models.Manager()  # 인식이 안되는 경우가 있어서 그냥 넣어줌
     isbn = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
     publisher = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    imgUrl = models.CharField(max_length=200)
+    imgUrl = models.CharField(max_length=200, null=True)
     introduce = models.CharField(max_length=500)
 
     def __str__(self):
--- Django/django_workspace/todos/book/views.py
+++ Django/django_workspace/todos/book/views.py
@@ -8,37 +8,49 @@ from book.serializer import BookSerializer
 
 @api_view(['GET'])
 def book_list(request):
-    books = Book.object.all()
+    books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
     return Response(serializer.data)
 
 
 @api_view(['GET'])
 def book_detail(request, pk):
-    book = Book.object.get(id=pk)
+    book = Book.objects.get(id=pk)
     serializer = BookSerializer(book, many=False)
     return Response(serializer.data)
 
 
 @api_view(['POST'])
+def book_search(request, keyword):
+    book = Book.objects.filter(title__icontains=keyword)
+    serializer = BookSerializer(book, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
 def book_create(request):
+    print(request.data)
     serializer = BookSerializer(data=request.data)  # 역직렬화
     if serializer.is_valid():  # 역직렬화일 경우 벨리드 체크 해줌
         serializer.save()
-    return Response({"message": "success"})
+    return Response(serializer.data)
+    # else:
+    #     return Response(serializer.data)
 
 
 @api_view(['PUT'])
 def book_update(request, pk):
-    book = Book.object.get(id=pk)
+    book = Book.objects.get(id=pk)
     serializer = BookSerializer(instance=book, data=request.data)
     if serializer.is_valid():
         serializer.save()
-    return Response({"message": "success"})
+        return Response({"message": "success"})
+    else:
+        return Response({"message": "failed"})
 
 
 @api_view(['DELETE'])
 def book_delete(request, pk):
-    book = Book.object.get(id=pk)
+    book = Book.objects.get(id=pk)
     book.delete()
     return Response({"message": "success"})

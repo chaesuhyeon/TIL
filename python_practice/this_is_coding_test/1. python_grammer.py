e7efa326e893e4f933fc313d020df76fa680a0ee
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2021-12-01 21:16:49 +0900

    python grammer

--- python_practice/this_is_coding_test/1. python_grammer.py
+++ python_practice/this_is_coding_test/1. python_grammer.py
@@ -59,6 +59,62 @@ answer = 7
 print(f"정답은 {answer}입니다.")
 
 
+# 조건문
+score = 85
+
+if score >= 70:
+    print('성적이 70점 이상입니다.')
+    if score >= 90:
+        print('우수한 성적 입니다')
+else:
+        print('성적이 70점 미만입니다')
+        print('조금 더 분발하세요')
+print('프로그램을 종료합니다.')
+
+
+# 논리 연산자
+if True or False:
+    print("yes") # yes라고 출력됨 . 둘중에 하나가 참이기 때문
+
+if True and False:
+    print("yes")  # 아무것도 출력 안됨. 둘다 참이 아니기 때문
+
+# 조건부 표현식
+score = 85
+if score >= 80: result = "Success"
+else : result = "Fail"
+print(result)
+
+
+score = 85
+result = "Success" if score >= 80 else "Fail"
+print(result)
+
+# 반복 while문
+i = 1
+result = 0
+while i <= 9:
+    result += i
+    i +=1
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
